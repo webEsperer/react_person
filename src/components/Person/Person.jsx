2018a3914324bbc18777b42db66b69d89acb9e75
Piotr Matejek <devmatejekpior@gmail.com>
2025-03-14 03:53:24 +0100

    Solution2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,7 @@ import './Person.scss';
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const kindOfSex = sex === 'f' ? 'husband' : 'wife';
+  const kindOfSex = sex === 'm' ? 'wife' : 'husband';
 
   const isPartner = isMarried ? (
     <p className="Person__partner">{`${partnerName} is my ${kindOfSex}`}</p>
